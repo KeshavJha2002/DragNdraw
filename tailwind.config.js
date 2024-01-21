2c7a1cb946848a56d3c1028b827d6502527f933b
Keshav Jha <jhak78022@gmail.com>
2024-01-21 19:01:54 +0530

    file structure updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#6965db",
+                primary_hover: "#5753d0",
+                primary_bg_color: "white",
+                secondary_bg_color: " #f0efff",
+                selected_bg_color: "#e0dfff",
+                primary_text_color: "#27272c",
+                secondary_text_color: "#b8b8b8",
+                highlight_color: "#030064",
+                border_color: "#e7e6f7",
+                shadow_color: "rgba(0, 0, 0, 0.1)",
+                hover_bg_color: "#e5e5fb",
+                panel_bg_color: "#e7e7ee"
+            }
+        },
     },
     plugins: [],
 }
